7115c4d471d27c32e6033c0c5e4bf57e1c9c4525
miha <Muxa1901@gmail.com>
2021-11-17 09:40:30 +0700

    commit

--- app/src/main/java/com/example/amgus/MainActivity.kt
+++ app/src/main/java/com/example/amgus/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.amgus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var login = findViewById<EditText>(R.id.login)
+        var loginText = login.text.toString()
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if (loginText != "sussybaka") {
+                login.error = "Неверный пароль."
+            }
+        }
     }
 }

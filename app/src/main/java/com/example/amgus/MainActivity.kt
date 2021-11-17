36d724cfe4952ae5212c9c861eecd344ad65462e
miha <Muxa1901@gmail.com>
2021-11-17 10:02:20 +0700

    commit

--- app/src/main/java/com/example/amgus/MainActivity.kt
+++ app/src/main/java/com/example/amgus/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var login = findViewById<EditText>(R.id.login)
+        var pass = findViewById<EditText>(R.id.pass)
         findViewById<Button>(R.id.button).setOnClickListener {
-            if (login.text.toString() != "sussybaka") {
-                login.error = "Неверный пароль."
+            if (login.text.toString() != "amgus") {
+                login.error = "Неверное имя пользователя."
+            } else if (pass.text.toString() != "sussybaka") {
+                pass.error = "Неверный пароль."
+            } else {
+                var myDialogFragment = DialogFragment()
+                var manager = supportFragmentManager
+                var builder = AlertDialog.Builder(it)
+                myDialogFragment.show(manager, "myDialog")
             }
         }
     }

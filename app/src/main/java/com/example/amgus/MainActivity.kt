b0975fa60599819c98a0c887dadb8a5969e2ef00
= <=>
2021-11-19 10:04:54 +0700

    added snackbars

--- app/src/main/java/com/example/amgus/MainActivity.kt
+++ app/src/main/java/com/example/amgus/MainActivity.kt
@@ -1,29 +1,68 @@
 package com.example.amgus
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.DialogFragment
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val duration = 10000
         var login = findViewById<EditText>(R.id.login)
         var pass = findViewById<EditText>(R.id.pass)
+        var container =
+            findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.container)
+        var count = 0
+
         findViewById<Button>(R.id.button).setOnClickListener {
-            if (login.text.toString() != "amgus") {
-                login.error = "Неверное имя пользователя."
-            } else if (pass.text.toString() != "sussybaka") {
-                pass.error = "Неверный пароль."
+            if (login.text.toString() != "amgus" || pass.text.toString() != "sussybaka") {
+                count++
+
+                var list = listOf<String>(
+                    "Неверные данные.",
+                    "Неправильно ввел.",
+                    "Ошибка: Неверно."
+                )
+                var str = list.random()
+                var laststr = ""
+                while (str == laststr) {
+                    laststr = list.random()
+                    continue
+                }
+                var snack = Snackbar.make(container, str, duration)
+                str.also { laststr = it }
+                var snack2 = Snackbar.make(container, "Я тебе помогу...", duration)
+                snack2.setAction("\$sudo --run hack.exe") {
+                    login.setText("amgus")
+                    pass.setText("sussybaka")
+                }
+
+                snack.setAction("Хорошо") {
+                    login.text.clear()
+                    pass.text.clear()
+                }
+
+                if (count >= 3) {
+                    snack.setAction("Взломать") {
+                        snack2.show()
+                    }
+                    count = 0
+                }
+                snack.show()
             } else {
-                var myDialogFragment = DialogFragment()
-                var manager = supportFragmentManager
-                var builder = AlertDialog.Builder(it)
-                myDialogFragment.show(manager, "myDialog")
+                var snack = Snackbar.make(container, "Чел харош", duration)
+                snack.setAction("Крутой") {
+                    login.text.clear()
+                    pass.text.clear()
+                }
+                snack.show()
             }
         }
     }

a408358aa1aece75dfb8caec207c6c966f2d964a
= <=>
2021-12-12 10:01:36 +0700

    added amongus

---
+++ app/src/main/java/com/example/amgus/MaterialActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.amgus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MaterialActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_material2)
+    }
+}

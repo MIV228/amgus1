3f55159fd191249bcff1093f9f936d891f99d13d
= <=>
2021-12-19 09:35:38 +0700

    added clicker activity

--- app/src/main/java/com/example/amgus/WatchActivity.kt
+++ app/src/main/java/com/example/amgus/WatchActivity.kt
@@ -2,8 +2,10 @@ package com.example.amgus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
@@ -16,13 +18,13 @@ class WatchActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editText)
         val checkBox = findViewById<CheckBox>(R.id.checkBox)
         val textView = findViewById<TextView>(R.id.textView)
+        var isChecked = PreferenceManager.getDefaultSharedPreferences(applicationContext).getBoolean("isChecked", false)
+        checkBox.isChecked = isChecked
+//        var editTextText = PreferenceManager.getDefaultSharedPreferences(applicationContext).getString("editTextText", "")
+//        editText.text.toString() = editTextText.toString()
 
         val textWatcher = object : TextWatcher {
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 if (s.contains("amgus")) {
                     textView.text = "SUS"
@@ -30,11 +32,9 @@ class WatchActivity : AppCompatActivity() {
                     editText.error = "Oh yeah"
                     textView.text = "ААААА МИШК ФРЕДЕ"
                 }
+//                PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putString("editTextText", s.toString()).apply()
             }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
+            override fun afterTextChanged(s: Editable?) {}
         }
 
         checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -44,6 +44,7 @@ class WatchActivity : AppCompatActivity() {
                 editText.removeTextChangedListener(textWatcher)
                 textView.text = ""
             }
+            PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putBoolean("isChecked", isChecked).apply()
         }
     }
 }
